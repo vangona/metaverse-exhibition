84a12addb424b35c5b4c1d01b2e39590662eca7e
vangona <rlarhksrud12@naver.com>
2022-01-21 22:46:28 +0900

    2022.01.21 keyboard control

--- src/components/home/Invitation.js
+++ src/components/home/Invitation.js
@@ -2,7 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
     position: absolute;
+    background-color: black;
+    color: white;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,5 @@
 import { Canvas, extend, useFrame, useThree } from '@react-three/fiber';
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Wall from '../components/Wall';
 import { OrbitControls, PointerLockControls } from '@react-three/drei';
 import Invitation from '../components/home/Invitation';
@@ -24,25 +24,43 @@ const CameraControls = () => {
 }
 
 const Home = () => {
+    const person = useRef();
     const [init, setInit] = useState(false);
+    const velocity = 0.01;
 
     const onInvitationClick = () => {
         setInit(true);
     }
+
+    const keyPress = (e) => {
+        if (e.keyCode === 87) {
+            person.current.moveForward(velocity);
+        } else if (e.keyCode === 83) {
+            person.current.moveForward(-velocity);
+        } else if (e.keyCode === 65) {
+            person.current.moveRight(-velocity);
+        } else if (e.keyCode === 68) {
+            person.current.moveRight(velocity);
+        }
+    }
+    
+    useEffect(() => {
+        window.addEventListener('keydown', keyPress)
+    }, [])
 
     return (
         <Container>                
             <Canvas style={{width: '100vw', height: '100vh'}}>
                 {init && 
                     <>
-                        <Wall position={[0, 0, 0]} rotation={[0, Math.PI / 2, 0]} scale={1} />
-                        <Wall position={[5, 0, 0]} rotation={[0, Math.PI / 2, 0]} scale={1} />
-                        <Wall position={[2.5, 0, -2.5]} scale={1} />
-                        <Wall position={[2.5, 0, 2.5]} scale={1} />
-                        <Wall position={[0, -2.5, 0]} rotation={[Math.PI / 2, 0, 0]} scale={5} />
+                        <Wall position={[0, 0, 0]} rotation={[0, Math.PI / 2, 0]} scale={0.1} />
+                        <Wall position={[0.5, 0, 0]} rotation={[0, Math.PI / 2, 0]} scale={0.1} />
+                        <Wall position={[0.25, 0, -0.25]} scale={0.1} />
+                        <Wall position={[0.25, 0, 0.25]} scale={0.1} />
+                        <Wall position={[0, -0.25, 0]} rotation={[Math.PI / 2, 0, 0]} scale={0.5} />
                     </>
                 }
-                <PointerLockControls />
+                <PointerLockControls ref={person} />
             </Canvas>  
             {!init && <Invitation onInvitationClick={onInvitationClick} />}
         </Container>
