7ff67267984ec7ea2b62ce21e6109e4b16a95fb6
vangona <rlarhksrud12@naver.com>
2022-01-27 16:50:21 +0900

    2022.01.27 practice cannon, threejs

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,96 +1,226 @@
-import { Canvas, extend, useFrame, useThree } from '@react-three/fiber';
 import React, { useEffect, useRef, useState } from 'react';
-import { OrbitControls, PointerLockControls } from '@react-three/drei';
-import Invitation from '../components/home/Invitation';
 import styled from 'styled-components';
-import { Physics, useBox, usePlane } from '@react-three/cannon';
- 
-extend({ OrbitControls });
-extend({ PointerLockControls });
 
-const Container = styled.div`
-    width: 95vw;
-    height: 95vh;
-`;
+import * as THREE from 'three';
+import CANNON from "cannon";
+import * as dat from "dat.gui";
+
+import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { AxesHelper, Vector3 } from 'three';
+
+const Container = styled.div``;
 
 const Home = () => {
-    const person = useRef();
-    const cameraRef = useRef();
-    const wallRef = useRef();
-    const [init, setInit] = useState(false);
-    const velocity = 0.01;
+    const mount = useRef();
 
-    const Floor = (props) => {
-        const [ref] = usePlane(() => ({ mass: 0, rotation: [-Math.PI / 2, 0, 0], ...props }))
+    function init() {
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+        const renderer = new THREE.WebGLRenderer();
+        renderer.setSize( window.innerWidth, window.innerHeight );
 
-        return (
-            <mesh ref={ref} receiveShadow>
-                <planeGeometry args={[1000, 1000]} />
-                <shadowMaterial color="#171717" transparent opacity={0.4} />
-                <meshLambertMaterial color="lightblue" />
-            </mesh>
+        renderer.shadowMap.enabled = true;
+
+        mount.current.appendChild( renderer.domElement );
+      
+
+        // cube
+        const cubeGeometry = new THREE.SphereGeometry( 1, 20, 20 );
+        const cubeMaterial = new THREE.MeshStandardMaterial( { color: 0x7e31eb } );
+        const cube = new THREE.Mesh( cubeGeometry, cubeMaterial );
+        cube.position.y = 3;
+        cube.castShadow = true;
+        // scene.add( cube );
+
+        // floor
+        const floorGeometry = new THREE.BoxGeometry( 100, 0.1, 100 );
+        const floorMaterial = new THREE.MeshStandardMaterial( { color: 'white' } );
+        const floor = new THREE.Mesh( floorGeometry, floorMaterial );
+        floor.rotation.y = Math.PI / 2;
+        floor.receiveShadow = true;
+
+        scene.add( floor );
+
+        const light = new THREE.DirectionalLight( 0xffffbb, 1 );
+
+        light.shadow.mapSize.width = 1024;
+        light.shadow.mapSize.height = 1024;
+
+        light.castShadow = true;
+
+        light.position.x = 2;
+        light.position.y = 10;
+        light.position.z = 2;
+        scene.add( light );
+
+        camera.position.x = 0.5;
+        camera.position.y = 0.5;
+        camera.position.z = 3;
+
+        const controls = new OrbitControls( camera, renderer.domElement );
+
+
+        const axesHelper = new THREE.AxesHelper( 5 );
+        scene.add( axesHelper );
+
+        // cannon world
+        const world = new CANNON.World();
+        world.gravity.set(0, -9.82, 0);
+
+
+        //material
+        const defaultMaterial = new CANNON.Material('default');
+        const concreteMaterial = new CANNON.Material('concrete');
+        const plasticMaterial = new CANNON.Material('plastic');
+
+        const defaultContactMaterial = new CANNON.ContactMaterial(
+            defaultMaterial,
+            defaultMaterial,
+            {
+                friction: 0.1,
+                restitution: 0.7
+            }
         )
-    }
 
-    const Cube = (props) => {
-        const [ref] = useBox(() => ({ mass: 1, position: [0, 0, 0], rotation: [0.4, 0.2, 0.5], ...props }))
+        world.addContactMaterial(defaultContactMaterial);
 
-        return (
-            <mesh receiveShadow castShadow ref={ref}>
-                <boxGeometry />
-                <meshLambertMaterial color="hotpink" />
-            </mesh>
+        const concretePlasticContactMaterial = new CANNON.ContactMaterial(
+            concreteMaterial,
+            plasticMaterial,
+            {
+                friction: 0.1,
+                restitution: 0.7
+            }
         )
-    }
 
-    const Wall = (props) => {
-        const [wallRef] = useBox(() => ({ mass: 1, position: [0, 0, 0], ...props }))
+        world.addContactMaterial(concretePlasticContactMaterial);
 
-        return (
-            <mesh ref={wallRef}>
-                <boxGeometry args={[5, 5]} />
-                <meshLambertMaterial color="black" />
-            </mesh>
-        )
-    }
+        world.defaultContactMaterial = defaultContactMaterial;
 
-    const onInvitationClick = () => {
-        setInit(true);
-    }
+        // objects
+        const sphereShape = new CANNON.Sphere(1);
+        const sphereBody = new CANNON.Body({
+            mass: 1,
+            position: new CANNON.Vec3(0, 3, 0),
+            shape: sphereShape,
+            material: defaultMaterial
+        })
 
-    const keyPress = (e) => {
-        if (e.keyCode === 87) {
-            person.current.moveForward(velocity);
-        } else if (e.keyCode === 83) {
-            person.current.moveForward(-velocity);
-        } else if (e.keyCode === 65) {
-            person.current.moveRight(-velocity);
-        } else if (e.keyCode === 68) {
-            person.current.moveRight(velocity);
+        sphereBody.applyLocalForce(new CANNON.Vec3(150, 0, 0), new CANNON.Vec3(0, 0, 0));
+
+        // world.addBody(sphereBody);
+
+        const floorShape = new CANNON.Plane();
+        const floorBody = new CANNON.Body();
+        floorBody.mass = 0;
+        floorBody.addShape( floorShape );
+        floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI / 2);
+
+        floorBody.material = defaultMaterial;
+
+        world.addBody(floorBody);
+
+        // body with time
+        const clock = new THREE.Clock();
+        let oldElapsedTime = 0;
+
+        const tick = () => {
+            const elapsedTime = clock.getElapsedTime();
+            const delataTime = elapsedTime - oldElapsedTime;
+            oldElapsedTime = elapsedTime;
+
+            world.step(1 / 60, delataTime, 3);
+
+            for (const object of objToUpdate) {
+                object.mesh.position.copy(object.body.position)
+            }
         }
+
+        // addEventListner
+        window.addEventListener('click', () => {
+            controls.lock();
+        })
+
+        // functions
+        const objToUpdate = [];
+        const createSphere = (radius, position, color) => {
+            // Three.js mesh
+            const mesh = new THREE.Mesh(
+                new THREE.SphereGeometry(radius, 20, 20),
+                new THREE.MeshStandardMaterial({
+                    metalness: 0.3,
+                    roughness: 0.4,
+                    color: color,
+                })
+            );
+            mesh.castShadow = true;
+            mesh.position.copy(position);
+            scene.add(mesh);
+
+            // Cannon.js body
+            const shape = new CANNON.Sphere(radius);
+
+            const body = new CANNON.Body({
+                mass: 1,
+                position: new CANNON.Vec3(0, 3, 0),
+                shape,
+                material: defaultMaterial,
+            });
+
+            body.applyLocalForce(
+                new CANNON.Vec3(
+                    Math.random()*100 + -50,
+                    0, 
+                    Math.random()*100 + -50,
+                ), 
+                new CANNON.Vec3(0, 0, 0))
+            body.position.copy(position);
+            world.addBody(body);
+
+            objToUpdate.push({ mesh, body });
+        }
+
+        // Dat.GUI
+
+        const gui = new dat.GUI();
+        const debugObject = {};
+
+        debugObject.createSphere = () => {
+            createSphere(
+                Math.random() * 0.3 + 0.2, 
+                { 
+                    x: (Math.random() - 0.5) * 3, 
+                    y: 3, 
+                    z: (Math.random() - 0.5) * 3 
+                },
+                Math.random() * 0xffffff
+            );
+        }
+
+        gui.add(debugObject, 'createSphere')
+
+        const animate = function() {
+            requestAnimationFrame( animate );
+            tick();
+            // cube.rotation.x += 0.01;
+            // cube.rotation.y += 0.01;
+            // cube.rotation.z += 0.01;
+
+            // controls.update();
+
+            renderer.render( scene, camera );
+        }
+
+        animate();
     }
 
     useEffect(() => {
-        window.addEventListener('keydown', keyPress);    
+           init();
     }, [])
 
     return (
-        <Container>                
-        <Canvas color={'blue'} shadows dpr={[1, 2]} gl={{ alpha: false }} camera={{ position: [-1, 5, 5], fov: 45 }}>
-                <ambientLight />
-                <directionalLight ref={wallRef} position={[10, 10, 10]} castShadow shadow-mapSize={[2048, 2048]} />
-                {init && 
-                    <Physics>
-                        <Wall position={[-1, 5, 1]} />
-                        <Floor position={[0, 0, 0]} />
-                        <Cube position={[-1, 10, 1]} />
-                        <Cube position={[-1, 20, 1]} />
-                        <Cube position={[-1, 30, 2]} />        
-                    </Physics>
-                }
-                <PointerLockControls camera position={[-1, 1, 1]} ref={person} />
-            </Canvas>
-            {!init && <Invitation onInvitationClick={onInvitationClick} />}
+        <Container ref={mount}>  
         </Container>
     );
 };
---
+++ src/routes/HomeR3F.js
@@ -0,0 +1,98 @@
+import { Canvas, extend, useFrame, useThree } from '@react-three/fiber';
+import React, { useEffect, useRef, useState } from 'react';
+import { OrbitControls, PointerLockControls } from '@react-three/drei';
+import Invitation from '../components/home/Invitation';
+import styled from 'styled-components';
+import { Physics, useBox, usePlane } from '@react-three/cannon';
+ 
+extend({ OrbitControls });
+extend({ PointerLockControls });
+
+const Container = styled.div`
+    width: 95vw;
+    height: 95vh;
+`;
+
+const Home = () => {
+    const person = useRef();
+    const cameraRef = useRef();
+    const wallRef = useRef();
+    const [init, setInit] = useState(false);
+    const velocity = 0.01;
+
+    const Floor = (props) => {
+        const [ref] = usePlane(() => ({ mass: 0, rotation: [-Math.PI / 2, 0, 0], ...props }))
+
+        return (
+            <mesh ref={ref} receiveShadow>
+                <planeGeometry args={[1000, 1000]} />
+                <shadowMaterial color="#171717" transparent opacity={0.4} />
+                <meshLambertMaterial color="lightblue" />
+            </mesh>
+        )
+    }
+
+    const Cube = (props) => {
+        const [ref] = useBox(() => ({ mass: 1, position: [0, 0, 0], rotation: [0.4, 0.2, 0.5], ...props }))
+
+        return (
+            <mesh receiveShadow castShadow ref={ref}>
+                <boxGeometry />
+                <meshLambertMaterial color="hotpink" />
+            </mesh>
+        )
+    }
+
+    const Wall = (props) => {
+        const [wallRef] = useBox(() => ({ mass: 1, position: [0, 0, 0], ...props }))
+
+        return (
+            <mesh ref={wallRef}>
+                <boxGeometry args={[5, 5]} />
+                <meshLambertMaterial color="black" />
+            </mesh>
+        )
+    }
+
+    const onInvitationClick = () => {
+        setInit(true);
+    }
+
+    const keyPress = (e) => {
+        if (e.keyCode === 87) {
+            person.current.moveForward(velocity);
+        } else if (e.keyCode === 83) {
+            person.current.moveForward(-velocity);
+        } else if (e.keyCode === 65) {
+            person.current.moveRight(-velocity);
+        } else if (e.keyCode === 68) {
+            person.current.moveRight(velocity);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('keydown', keyPress);    
+    }, [])
+
+    return (
+        <Container>                
+        <Canvas color={'blue'} shadows dpr={[1, 2]} gl={{ alpha: false }} camera={{ position: [-1, 5, 5], fov: 45 }}>
+                <ambientLight />
+                <directionalLight ref={wallRef} position={[10, 10, 10]} castShadow shadow-mapSize={[2048, 2048]} />
+                {init && 
+                    <Physics>
+                        <Wall position={[-1, 5, 1]} />
+                        <Floor position={[0, 0, 0]} />
+                        <Cube position={[-1, 10, 1]} />
+                        <Cube position={[-1, 20, 1]} />
+                        <Cube position={[-1, 30, 2]} />        
+                    </Physics>
+                }
+                <PointerLockControls camera sphere position={[-1, 1, 1]} ref={person} />
+            </Canvas>
+            {!init && <Invitation onInvitationClick={onInvitationClick} />}
+        </Container>
+    );
+};
+
+export default Home;
